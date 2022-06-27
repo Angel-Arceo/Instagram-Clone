5bef00b22da4d0cb4d7726eff330573e533b2ff8
Angel-Arceo <arceoamayaangel@gmail.com>
2022-06-27 14:37:07 -0500

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send('Hello World')
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
