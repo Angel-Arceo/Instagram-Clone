2d1b6e0b703fdc725bce5cf028457e8f98f36dd1
Angel-Arceo <arceoamayaangel@gmail.com>
2022-07-18 21:28:35 -0500

    adding a link to the bottomNav component

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -21,7 +21,9 @@ const BottomNav = () => {
                     </Link>
                 </li>
                 <li>
-                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.3" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="7" r="4" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                    <Link to="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.3" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="7" r="4" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                    </Link>
                 </li>
             </ul>
         </nav>
