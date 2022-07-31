8d3a3e3d91c92b44c5cdc635ff36e9dcf8a32009
Angel-Arceo <arceoamayaangel@gmail.com>
2022-07-30 22:15:52 -0500

    useFetch custom hook added

--- client/src/components/panel/Persons.js
+++ client/src/components/panel/Persons.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Person from './Person';
+import { Link } from 'react-router-dom';
 
 const Persons = ({persons}) => {
     return (
         <div className="flex justify-center mx-2 overflow-x-auto">
-            {persons.map(person => <Person key={person.id} profileUser={person.profile} username={person.username} />)}
+            {
+            persons.map(person => 
+            <Link to="/">
+                <Person key={person.id} profileUser={person.profile} username={person.username} />
+            </Link>)
+            }
         </div>
     );
 }
